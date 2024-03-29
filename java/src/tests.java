658f89a09e42d5baf43b0f8ee1a6f37f228f0514
AlfonsoFdzGrby <159499925+AlfonsoFdzGrby@users.noreply.github.com>
2024-03-28 23:41:31 -0600

    Delete java/src/tests.java

--- java/src/tests.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class tests {
-    public static void main(String[] args) {
-        LocalDate fecha = LocalDate.of(2025, 3, 25);
-        System.out.println(fecha.toString());
-    }
-}
